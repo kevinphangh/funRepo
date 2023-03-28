b9aa3044b9cb709789d9800a4d8812559c6b5f47
kevinphangh <99958493+kevinphangh@users.noreply.github.com>
2023-03-28 15:48:55 +0200

    Working

--- search/src/app/product-search/product-search.component.ts
+++ search/src/app/product-search/product-search.component.ts
@@ -13,7 +13,7 @@ export class ProductSearchComponent {
   searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
 
   onSearchTextChanged(){
-    this.searchTextChanged.emit(this.enteredSearchValue); 
+    this.searchTextChanged.emit(this.enteredSearchValue.toLowerCase()); 
   }
 
 }
--- search/src/app/products/products.component.ts
+++ search/src/app/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../../assets/products.d';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -11,19 +11,48 @@ import { map } from 'rxjs/operators';
 })
 
 export class ProductsComponent {
-  currentPage: number = 1; 
-  productsPerPage: number = 10; 
+  currentPage: number = 1;
+  productsPerPage: number = 10;
   products: Observable<Product[]> = of([]);
   searchText: string = '';
+  firstSearch: boolean = true;
 
   constructor(private http: HttpClient) {
-    this.products = this.http.get<any>('assets/products.json')
-      .pipe(
-        map(data => data.content)
-      );
   }
 
   onSearchTextEntered(searchValue: string) {
+    if (this.firstSearch) {
+      this.products = this.http.get<any>('assets/products.json')
+        .pipe(
+          map(data => data.content)
+        );
+      this.firstSearch = false;
+    }
+    this.currentPage = 1;
     this.searchText = searchValue;
+  }
+
+  filteredAndSlicedProducts(products: Product[]): Product[] {
+    const filteredProducts = products.filter(product =>
+      this.searchText === '' || product.title.toLowerCase().includes(this.searchText)
+    );
+    return filteredProducts.slice(
+      (this.currentPage - 1) * this.productsPerPage,
+      this.currentPage * this.productsPerPage
+    );
+  }
+
+  getTotalPages(products: Product[]): number {
+    const filteredProducts = products.filter(product =>
+      this.searchText === '' || product.title.toLowerCase().includes(this.searchText)
+    );
+    let totalPages: number = Math.ceil(filteredProducts.length / this.productsPerPage);
+    if(totalPages > 0) return totalPages
+    return 1; 
+  }
+    
+
+  goToPage(page: number) {
+    this.currentPage = page;
   }
 }
